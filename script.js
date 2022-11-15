defdc20becd52a93b4810f4cae805aaf8398328b
Ankur0661 <ankurk171@gmail.com>
2022-11-15 20:24:13 +0530

    some changes

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function () {
 
   // typing text animation script
   var typed = new Typed(".typing", {
-    strings: ["Back-End Developer", "Team Player"],
+    strings: ["Back-End Developer", "Problem Solver"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
